cafbcb7605b3aaa93d619ab4a7ec2129cd54c954
欧阳洲 <allenwill211@gmail.com>
2017-01-04 18:22:07 +0800

    change Conding

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass');
 var fileinclude = require('gulp-file-include');
-// var include = require('gulp-include'); //html代码复用 1
-var concat = require('gulp-concat'); //文件合并插件 1
-// var imagemin = require('gulp-imagemin');         //图片压缩 1
-var babel = require('gulp-babel'); //编译es6 1
-var uglify = require('gulp-uglify'); //Js 文件压缩 1
-var autoprefixer = require('gulp-autoprefixer'); //css3自动添加前缀
-// var shrink = require('gulp-cssshrink'); //css 文件压缩 1
-// var zip = require('gulp-zip');                   // gulp压缩工具 1
+// var include = require('gulp-include');         //html代码复用 
+var concat = require('gulp-concat');              //文件合并插件 
+// var imagemin = require('gulp-imagemin');       //图片压缩 
+var babel = require('gulp-babel');                //编译es6 
+var uglify = require('gulp-uglify');              //Js 文件压缩 
+var connect = require('gulp-connect');            //自动刷新
+var autoprefixer = require('gulp-autoprefixer');  //css3自动添加前缀
+// var shrink = require('gulp-cssshrink');        //css 文件压缩 1
+// var zip = require('gulp-zip');                 // gulp压缩工具 1
 
 
 // 设置监听任务
-gulp.task('default', ['scss', 'es6', 'images', 'html', 'vendors'], function() {});
+gulp.task('default', ['scss', 'es6', 'images', 'html', 'vendors', 'connect'], function() {});
+
+
 // 监听变化
 gulp.task('watch', ['default'], function() {
     gulp.watch('./src/scss/**/*.scss', ['scss']);
@@ -20,6 +23,22 @@ gulp.task('watch', ['default'], function() {
     gulp.watch('./src/js/**/*.js', ['es6']);
     gulp.watch('./src/images/**/*.*', ['images']);
 })
+
+// 自动刷新
+gulp.task('connect', function() {
+        connect.server({
+        // host: '192.168.1.172', //地址，可不写，不写的话，默认localhost
+        port: 1230, //端口号，可不写，默认8000
+        root: './', //当前项目主目录
+        livereload: true //自动刷新
+    })
+})
+
+// gulp.task('watch', function() {
+//     gulp.watch('./src/scss/style.scss', ['html']); //监控css文件
+//     gulp.watch('./src/js/**/*.js', ['html']); //监控js文件
+//     gulp.watch(['./src/html/webPage/*.html'], ['html']); //监控html文件
+// }); //执行gulp server开启服务器
 
 // 编译scss
 gulp.task('scss', function() {
@@ -34,6 +53,7 @@ gulp.task('scss', function() {
         .pipe(gulp.dest('./dist/css'));
 })
 
+
 // 编译
 gulp.task('html', function() {
     return gulp.src('./src/html/webPage/*.html')
@@ -41,6 +61,7 @@ gulp.task('html', function() {
           prefix: '@@',
           basepath: '@file'
         }))
+        .pipe(connect.reload())
         .on('error', console.log) //监听错误， 打印日志
         .pipe(gulp.dest('./dist/html'));
 })
@@ -60,6 +81,7 @@ gulp.task('es6', function() {
         // .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 });
+
 
 //插件应用库
 gulp.task('vendors', function() {
